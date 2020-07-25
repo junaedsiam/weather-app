fede4f8b14109f9092860d5839486f390e0d0cf6
junaedsiam <junaedsiam@gmail.com>
2020-07-25 20:48:00 +0600

    heroku setup done

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const forecast = require("./forecast");
 const geoCode = require("./geocode");
 //initiation 
 const app = express();
-const port = +process.argv[2] || 4000;  
+const port = process.env.PORT || 3000
 
 //middlewares
 app.use(express.static('public'))
